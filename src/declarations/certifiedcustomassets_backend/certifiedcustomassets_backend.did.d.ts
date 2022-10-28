d6cf99148cc93ae7dc16e9b0a304e603050d640e
David Dal Busco <david.dalbusco@outlook.com>
2022-10-28 11:20:31 +0200

    feat: initial import (no certification)

---
+++ src/declarations/certifiedcustomassets_backend/certifiedcustomassets_backend.did.d.ts
@@ -0,0 +1,62 @@
+import type { Principal } from '@dfinity/principal';
+import type { ActorMethod } from '@dfinity/agent';
+
+export interface AssetKey {
+  'token' : [] | [string],
+  'sha256' : [] | [Array<number>],
+  'name' : string,
+  'fullPath' : string,
+  'folder' : string,
+}
+export interface Chunk { 'content' : Array<number>, 'batchId' : bigint }
+export interface CommitBatch {
+  'headers' : Array<[string, string]>,
+  'chunkIds' : Array<bigint>,
+  'batchId' : bigint,
+}
+export interface Del { 'token' : [] | [string], 'fullPath' : string }
+export interface HttpRequest {
+  'url' : string,
+  'method' : string,
+  'body' : Array<number>,
+  'headers' : Array<[string, string]>,
+}
+export interface HttpResponse {
+  'body' : Array<number>,
+  'headers' : Array<[string, string]>,
+  'streaming_strategy' : [] | [StreamingStrategy],
+  'status_code' : number,
+}
+export interface InitUpload { 'batchId' : bigint }
+export interface StreamingCallbackHttpResponse {
+  'token' : [] | [StreamingCallbackToken],
+  'body' : Array<number>,
+}
+export interface StreamingCallbackToken {
+  'token' : [] | [string],
+  'sha256' : [] | [Array<number>],
+  'fullPath' : string,
+  'headers' : Array<[string, string]>,
+  'index' : bigint,
+}
+export type StreamingStrategy = {
+    'Callback' : {
+      'token' : StreamingCallbackToken,
+      'callback' : [Principal, string],
+    }
+  };
+export interface UploadChunk { 'chunkId' : bigint }
+export interface _SERVICE {
+  'commitUpload' : ActorMethod<[CommitBatch], undefined>,
+  'cyclesBalance' : ActorMethod<[], bigint>,
+  'del' : ActorMethod<[Del], undefined>,
+  'http_request' : ActorMethod<[HttpRequest], HttpResponse>,
+  'http_request_streaming_callback' : ActorMethod<
+    [StreamingCallbackToken],
+    StreamingCallbackHttpResponse,
+  >,
+  'initUpload' : ActorMethod<[AssetKey], InitUpload>,
+  'list' : ActorMethod<[[] | [string]], Array<AssetKey>>,
+  'transferFreezingThresholdCycles' : ActorMethod<[], undefined>,
+  'uploadChunk' : ActorMethod<[Chunk], UploadChunk>,
+}
---
+++ src/declarations/certifiedcustomassets_backend/certifiedcustomassets_backend.did.js
@@ -0,0 +1,69 @@
+export const idlFactory = ({ IDL }) => {
+  const CommitBatch = IDL.Record({
+    'headers' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+    'chunkIds' : IDL.Vec(IDL.Nat),
+    'batchId' : IDL.Nat,
+  });
+  const Del = IDL.Record({
+    'token' : IDL.Opt(IDL.Text),
+    'fullPath' : IDL.Text,
+  });
+  const HttpRequest = IDL.Record({
+    'url' : IDL.Text,
+    'method' : IDL.Text,
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+  });
+  const StreamingCallbackToken = IDL.Record({
+    'token' : IDL.Opt(IDL.Text),
+    'sha256' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'fullPath' : IDL.Text,
+    'headers' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+    'index' : IDL.Nat64,
+  });
+  const StreamingStrategy = IDL.Variant({
+    'Callback' : IDL.Record({
+      'token' : StreamingCallbackToken,
+      'callback' : IDL.Func([], [], []),
+    }),
+  });
+  const HttpResponse = IDL.Record({
+    'body' : IDL.Vec(IDL.Nat8),
+    'headers' : IDL.Vec(IDL.Tuple(IDL.Text, IDL.Text)),
+    'streaming_strategy' : IDL.Opt(StreamingStrategy),
+    'status_code' : IDL.Nat16,
+  });
+  const StreamingCallbackHttpResponse = IDL.Record({
+    'token' : IDL.Opt(StreamingCallbackToken),
+    'body' : IDL.Vec(IDL.Nat8),
+  });
+  const AssetKey = IDL.Record({
+    'token' : IDL.Opt(IDL.Text),
+    'sha256' : IDL.Opt(IDL.Vec(IDL.Nat8)),
+    'name' : IDL.Text,
+    'fullPath' : IDL.Text,
+    'folder' : IDL.Text,
+  });
+  const InitUpload = IDL.Record({ 'batchId' : IDL.Nat });
+  const Chunk = IDL.Record({
+    'content' : IDL.Vec(IDL.Nat8),
+    'batchId' : IDL.Nat,
+  });
+  const UploadChunk = IDL.Record({ 'chunkId' : IDL.Nat });
+  return IDL.Service({
+    'commitUpload' : IDL.Func([CommitBatch], [], []),
+    'cyclesBalance' : IDL.Func([], [IDL.Nat], ['query']),
+    'del' : IDL.Func([Del], [], []),
+    'http_request' : IDL.Func([HttpRequest], [HttpResponse], ['query']),
+    'http_request_streaming_callback' : IDL.Func(
+        [StreamingCallbackToken],
+        [StreamingCallbackHttpResponse],
+        ['query'],
+      ),
+    'initUpload' : IDL.Func([AssetKey], [InitUpload], []),
+    'list' : IDL.Func([IDL.Opt(IDL.Text)], [IDL.Vec(AssetKey)], ['query']),
+    'transferFreezingThresholdCycles' : IDL.Func([], [], []),
+    'uploadChunk' : IDL.Func([Chunk], [UploadChunk], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
